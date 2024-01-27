fe9146f4a990af41e2fb9952f32adbe0f5db3ce2
Julien Thomas <jthomas@zenetys.com>
2024-01-27 11:33:29 +0100

    MAJOR: libzcurl: Rework lua-curl helper library for coming libzcurlexec

--- libzcurl.lua
+++ libzcurl.lua
@@ -3,49 +3,127 @@
 -- Author: Julien Thomas
 -- Licence: MIT
 
-local curl = require 'lcurl'
+local curl = require 'lcurl.safe'
+local lu = require 'libutil'
+
+local curlinfo_allowed = {
+    INFO_APPCONNECT_TIME = true,
+    INFO_CONNECT_TIME = true,
+    INFO_CONTENT_LENGTH_DOWNLOAD = true,
+    INFO_CONTENT_LENGTH_UPLOAD = true,
+    INFO_CONTENT_TYPE = true,
+    INFO_COOKIELIST = true,
+    INFO_EFFECTIVE_URL = true,
+    INFO_HEADER_SIZE = true,
+    INFO_HTTP_VERSION = true,
+    INFO_LOCAL_IP = true,
+    INFO_NAMELOOKUP_TIME = true,
+    INFO_NUM_CONNECTS = true,
+    INFO_PRIMARY_IP = true,
+    INFO_PRIMARY_PORT = true,
+    INFO_PROTOCOL = true,
+    INFO_REDIRECT_COUNT = true,
+    INFO_REDIRECT_TIME = true,
+    INFO_REDIRECT_URL = true,
+    INFO_REQUEST_SIZE = true,
+    INFO_SCHEME = true,
+    INFO_SIZE_DOWNLOAD = true,
+    INFO_SIZE_UPLOAD = true,
+    INFO_SPEED_DOWNLOAD = true,
+    INFO_SPEED_UPLOAD = true,
+    INFO_STARTTRANSFER_TIME = true,
+    INFO_RESPONSE_CODE = true,
+    INFO_TOTAL_TIME = true,
+}
 
 local ZCurl = {}
+ZCurl.__index = ZCurl
 
-function ZCurl:new(curlopts)
+ZCurl.curlopts_function = {}
+ZCurl.curlinfo_enabled = {}
+for k,v in pairs(curl) do
+    if k:sub(1, 4) == 'OPT_' and k:sub(-8) == 'FUNCTION' then
+        local opt_id = math.floor(v)
+        local opt_name = k:sub(5):lower()
+        ZCurl.curlopts_function[opt_id] = true
+        ZCurl.curlopts_function[opt_name] = true
+    elseif k:sub(1, 5) == 'INFO_' then
+        ZCurl.curlinfo_enabled[k] = curlinfo_allowed[k] or false
+    end
+end
+
+
+function ZCurl.new(curlopts)
     local instance = {}
-    self.__index = self
-    setmetatable(instance, self)
+    setmetatable(instance, ZCurl)
+
     instance.default_curlopts = curlopts
-    instance.curl = curl.easy(curlopts)
-    instance.response = {}
-    return instance
+    instance.current_curlopts = nil
+    instance.response = nil
+    instance.info = nil
+
+    local err
+    instance.curl, err = curl.easy()
+    if not instance.curl then return nil, err end
+    return instance:resetopts()
+end
+
+function ZCurl:o(opt, nil_value)
+    if type(opt) == 'string' and opt:sub(1,6) ~= 'zcurl_' then
+        local v = self.current_curlopts[curl['OPT_'..opt:upper()]]
+        if v then return v end
+    end
+    return self.current_curlopts[opt] or nil_value
 end
 
 function ZCurl:resetopts()
-    self.curl:reset()
-    self:setopts(self.default_curlopts)
+    ret, err = self.curl:reset()
+    if not ret then return nil, tostring(err) end
+    self.current_curlopts = {}
+    return self:setopts(self.default_curlopts)
 end
 
 function ZCurl:setopts(setcurlopts, unsetcurlopts)
     if setcurlopts then
-        self.curl:setopt(setcurlopts)
-    end
-    if unsetcurlopts then
-        for i = 1, #unsetcurlopts do
-            self.curl:unsetopt(unsetcurlopts[i])
+        for o,v in pairs(setcurlopts) do
+            if ZCurl.curlopts_function[o] and type(v) == 'function' then
+                v = lu.fbind(v, self)
+            end
+            ret, err = self.curl:setopt({ [o] = v })
+            if not ret then return nil, tostring(err) end
+            self.current_curlopts[o] = v
         end
     end
+    if unsetcurlopts then
+        for _,o in ipairs(unsetcurlopts) do
+            ret, err = self.curl:unsetopt(o)
+            if not ret then return nil, tostring(err) end
+            self.current_curlopts[o] = nil
+        end
+    end
+    return self
 end
 
 function ZCurl:perform(setcurlopts, unsetcurlopts)
-    -- reset response buffers
+    local pret, ret, err
+    -- reset response and info storage
     self.response = {}
+    self.info = {}
     -- update options
-    self:setopts(setcurlopts, unsetcurlopts)
-    -- call curl
-    local success, err = pcall(self.curl.perform, self.curl)
-    if success then return true end
-    -- build ready to print error message
-    local code = self:getinfo(curl.INFO_RESPONSE_CODE)
-    local msg = 'cURL failed: '
-    if code ~= nil and code > 0 then msg = msg..('HTTP status %d, '):format(code) end
-    return false, msg..err:msg()
+    ret, err = self:setopts(setcurlopts, unsetcurlopts)
+    if not ret then return nil, tostring(err) end
+    -- run request, note pcall protects from lua-curl error, eg: lua error
+    -- in a setopt_*function callback
+    pret, ret, err = pcall(self.curl.perform, self.curl)
+    if not pret then err = ret; ret = nil end
+    -- fill info data
+    for k,v in pairs(ZCurl.curlinfo_enabled) do
+        if v then self.info[k:sub(6):lower()] = self:getinfo(curl[k]) end
+    end
+    if ret then return self end
+    return nil, (self.info.response_code and self.info.response_code > 0
+        and ('HTTP status %d, '):format(self.info.response_code) or '')..
+        tostring(err)
 end
 
 function ZCurl:close()
@@ -56,21 +134,27 @@ function ZCurl:getinfo(info)
     return self.curl:getinfo(info)
 end
 
--- Usage: set OPT_WRITEFUNCTION to ZCurl._write_quiet
-function ZCurl._write_quiet(x)
-    return x and x:len() or 0
-end
-
--- Usage: set OPT_WRITEFUNCTION to <instance>:_write_response_buffer('body')
--- Usage: set OPT_HEADERFUNCTION to <instance>:_write_response_buffer('header')
--- Retrieve data from the <instance>.reponse.<name>
--- Note: response buffers are reset on each perform() calls.
-function ZCurl:_write_response_buffer(name)
-    local this = self;
-    return function (x)
-        this.response[name] = (this.response[name] or '')..x
+function ZCurl._store_response(name)
+    return function (self, x)
+        if name then self.response[name] = (self.response[name] or '')..x end
         return x and x:len() or 0
     end
+end
+
+-- ZCurl* modules advertise themself with ZCurl.register_abstract().
+-- ZCurl.new_abstract() will instanciate the first module whose condition succeeds.
+local abstract_modules = {}
+function ZCurl.register_abstract(class, condition_fn)
+    table.insert(abstract_modules, { class = class, condition_fn = condition_fn })
+end
+function ZCurl.new_abstract(...)
+    local args = {...}
+    for _,m in ipairs(abstract_modules) do
+        if m.condition_fn(table.unpack(args)) then
+            return m.class.new(table.unpack(args))
+        end
+    end
+    return ZCurl.new(table.unpack(args))
 end
 
 return ZCurl
